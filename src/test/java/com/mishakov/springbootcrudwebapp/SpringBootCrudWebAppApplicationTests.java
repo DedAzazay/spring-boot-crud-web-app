453cb4a226e878659fdac66ec2267ce2bec1233a
Dmitry Mishakov <welzevul0@gmail.com>
2022-04-14 19:46:39 +0300

    Init commit

---
+++ src/test/java/com/mishakov/springbootcrudwebapp/SpringBootCrudWebAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mishakov.springbootcrudwebapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootCrudWebAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
